29f39abff631d304f17a2239117dad2bc2365acb
alexsandersarmento <alexsander.br18@gmail.com>
2023-04-27 14:02:25 -0300

    feat: configuring the root layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
-import { Slot } from 'expo-router'
+import {
+  SplashScreen,
+  Slot,
+} from 'expo-router'
+import { useFonts, Inter_500Medium } from '@expo-google-fonts/inter'
 
 import { AppProvider } from '../contexts/AppProvider'
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Inter_500Medium,
+  })
+
+  if (!fontsLoaded) {
+    return <SplashScreen />
+  }
+
   return (
     <AppProvider>
       <Slot />
