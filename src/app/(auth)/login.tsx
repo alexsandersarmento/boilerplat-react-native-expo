f35bb457d44b637563e25c24a047650f3951218a
alexsandersarmento <alexsander.br18@gmail.com>
2023-04-30 13:46:20 -0300

    feat: added login with Google

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState,  useEffect } from 'react'
 import { Button, Center, VStack, useColorModeValue } from 'native-base'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm, FormProvider } from 'react-hook-form'
 import { useRouter } from 'expo-router'
 import auth from '@react-native-firebase/auth'
+import { GoogleSignin } from '@react-native-google-signin/google-signin'
 
+import GoogleSigninButton from '../../components/GoogleSigninButton'
 import Form from '../../components/Form'
 import { useAuth } from '../../hooks/useAuth'
 
@@ -25,6 +27,8 @@ const loginSchema = z.object({
 type TLoginData = z.infer<typeof loginSchema>
 
 export default function Login() {
+  const [isGoogleSigninLoading, setIsGoogleSigninLoading] = useState(false)
+
   const loginForm = useForm<TLoginData>({
     resolver: zodResolver(loginSchema),
   })
@@ -44,6 +48,29 @@ export default function Login() {
     }
   }
 
+  const handleLoginWithGoogle = async () => {
+    try {
+      setIsGoogleSigninLoading(true)
+
+      await GoogleSignin.hasPlayServices()
+      const { idToken } = await GoogleSignin.signIn()
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken)
+      const { user } = await auth().signInWithCredential(googleCredential)
+
+      login(user)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setIsGoogleSigninLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: '1066131766845-kcon415bejif60epslrb86bkbmgm2mg4.apps.googleusercontent.com',
+    })
+  }, [])
+
   return (
     <Center flex={1} bg={useColorModeValue('gray.50', 'gray.800')}>
       <VStack space={2} w='90%'>
@@ -62,6 +89,7 @@ export default function Login() {
         <Button onPress={handleSubmit(handleLogin)} isLoading={isSubmitting} w='full'>
           Login
         </Button>
+        <GoogleSigninButton onPress={handleLoginWithGoogle} isLoading={isGoogleSigninLoading}/>
         <Button onPress={() => navigation.push('/signUp')} variant='outline' w='full'>
           Sign Up
         </Button>
