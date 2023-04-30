b7fd343d4aa74d445317719f95a91f12c2890f75
alexsandersarmento <alexsander.br18@gmail.com>
2023-04-30 13:46:49 -0300

    feat: check if user is logged with Google on logout

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,6 +2,7 @@ import React, { createContext, useEffect } from 'react'
 import { MMKV, useMMKVObject } from 'react-native-mmkv'
 import { useRouter, useSegments } from 'expo-router'
 import auth from '@react-native-firebase/auth'
+import { GoogleSignin } from '@react-native-google-signin/google-signin'
 
 interface IUser {
   displayName: string | null;
@@ -45,7 +46,12 @@ export const AuthProvider: React.FC<IAuthContextProvider> = ({ children }) => {
     setUser(data)
   }
 
-  const logout = () => {
+  const logout = async () => {
+    const isGoogleUser = await GoogleSignin.isSignedIn()
+    if (isGoogleUser) {
+      GoogleSignin.signOut()
+    }
+    
     auth().signOut().then(() => {
       storage.delete('user')
     }).catch(error => {
