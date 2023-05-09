746d6575f5fad4fca48cefe1e264ac174ec6827c
alexsandersarmento <alexsander.br18@gmail.com>
2023-05-09 18:25:00 -0300

    feat: exported firebase database

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,7 +1,9 @@
 import auth from '@react-native-firebase/auth'
 import storage from '@react-native-firebase/storage'
+import database from '@react-native-firebase/database'
 
 export const firebase = {
   auth,
   storage,
+  database,
 }
