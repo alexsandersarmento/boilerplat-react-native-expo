c9fc102f6fa62ea083c6b6204921df863f50ca08
alexsandersarmento <alexsander.br18@gmail.com>
2023-05-13 17:55:28 -0300

    feat: added safe area around the chat header

--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -1,47 +1,51 @@
 import React from 'react'
-import { HStack, VStack, Image, Text, Pressable, Icon, Center } from 'native-base'
+import { HStack, VStack, Image, Text, Pressable, Icon, Center, useTheme } from 'native-base'
 import { useSearchParams, useRouter } from 'expo-router'
 import { Entypo } from '@expo/vector-icons'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function ChatHeader() {
   const params = useSearchParams()
   const router = useRouter()
+  const { colors } = useTheme()
 
   const handleGoBack = () => {
     router.back()
   }
 
   return (
-    <Center
-      width='full'
-      height={110}
-      justifyContent='center'
-      paddingBottom={2}
-      bg='muted.50'
-    >
-      <Pressable 
-        onPress={handleGoBack} 
-        position='absolute'
-        left={4}
+    <SafeAreaView style={{
+      flex: 1,
+      backgroundColor: colors.muted[50],
+    }}>
+      <Center
+        height='70px'
+        bg='muted.50'
       >
-        <Icon as={<Entypo name="chevron-thin-left"/>} size={6}/>
-      </Pressable>
-      <VStack>
-        <HStack alignItems='center' >
-          <Image
-            source={{ uri: decodeURIComponent(params.otherUserAvatar as string) }}
-            alt='Avatar'
-            size={8}
-            borderRadius='full'
-          />
-          <Text ml={2} fontSize={16} fontWeight='bold'>
-            {params.otherUserName}
+        <Pressable 
+          onPress={handleGoBack} 
+          position='absolute'
+          left={4}
+        >
+          <Icon as={<Entypo name="chevron-thin-left"/>} size={6}/>
+        </Pressable>
+        <VStack>
+          <HStack alignItems='center' >
+            <Image
+              source={{ uri: decodeURIComponent(params.otherUserAvatar as string) }}
+              alt='Avatar'
+              size={8}
+              borderRadius='full'
+            />
+            <Text ml={2} fontSize={16} fontWeight='bold'>
+              {params.otherUserName}
+            </Text>
+          </HStack>
+          <Text fontSize={12} color='muted.400'>
+            {params.otherUserStatus}
           </Text>
-        </HStack>
-        <Text fontSize={12} color='muted.400'>
-          {params.otherUserStatus}
-        </Text>
-      </VStack>
-    </Center>
+        </VStack>
+      </Center>
+    </SafeAreaView>
   )
 }
