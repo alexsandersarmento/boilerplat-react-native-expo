ceaffcb3d6c2dcd4f1f4e68cb631a044879ca427
alexsandersarmento <alexsander.br18@gmail.com>
2023-05-15 22:32:59 -0300

    refactor: improve the code of the chat header

--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -1,34 +1,30 @@
 import React from 'react'
-import { HStack, VStack, Image, Text, Pressable, Icon, Center, useTheme } from 'native-base'
+import { HStack, VStack, Image, Text, Icon, useColorModeValue } from 'native-base'
 import { useSearchParams, useRouter } from 'expo-router'
-import { Entypo } from '@expo/vector-icons'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 export default function ChatHeader() {
   const params = useSearchParams()
   const router = useRouter()
-  const { colors } = useTheme()
 
   const handleGoBack = () => {
     router.back()
   }
 
   return (
-    <SafeAreaView style={{
-      flex: 1,
-      backgroundColor: colors.muted[50],
-    }}>
-      <Center
-        height='70px'
-        bg='muted.50'
+      <HStack
+        alignItems="center"
+        justifyContent="space-between"
+        safeAreaTop
+        bg={useColorModeValue('muted.50', 'gray.800')}
+        px={4}
+        py={2}
       >
-        <Pressable 
-          onPress={handleGoBack} 
-          position='absolute'
-          left={4}
-        >
-          <Icon as={<Entypo name="chevron-thin-left"/>} size={6}/>
-        </Pressable>
+        <Icon
+          size={8}
+          as={<MaterialCommunityIcons name="chevron-left" />}
+          onPress={handleGoBack}
+        />
         <VStack>
           <HStack alignItems='center' >
             <Image
@@ -45,7 +41,7 @@ export default function ChatHeader() {
             {params.otherUserStatus}
           </Text>
         </VStack>
-      </Center>
-    </SafeAreaView>
+        <Icon size="sm" />
+      </HStack>
   )
 }
